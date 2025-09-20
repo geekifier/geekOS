f0953049444a589fec882edcf85d19f20f9b4736
Kamil Markowicz <kamil@markowicz.net>
2025-09-20 17:29:02 -0400

    chore(build): refactor package install
    This change prevents repos used to install extra packages from being
    enabled in the final image. End users are not expected to update these
    packages manually.

--- build_files/110-desktop_packages.sh
+++ build_files/110-desktop_packages.sh
@@ -5,17 +5,28 @@ echo "::group:: ===$(basename "$0")==="
 set -ouex pipefail
 
 packages=(
-    brave-browser
-    code
-    coolercontrol
-    ghostty
-    mpv-nightly
+  "code:code"
+  "brave-browser:brave-browser"
+  "terra:coolercontrol"
+  "terra:ghostty"
+  "terra:mpv-nightly"
 )
 
-if [[ "${#packages[@]}" -gt 0 ]]; then
-    dnf5 install -yq "${packages[@]}"
-else
-    echo "No packages to install"
-fi
+((${#packages[@]})) || {
+  echo "No packages to install"
+  exit 0
+}
+
+# CSV of unique repos, which can be supplied to --enablerepo
+repos_csv=$(
+  printf '%s\n' "${packages[@]}" |
+    awk -F: '{print $1}' |
+    sort -u |
+    paste -sd,
+)
+
+mapfile -t pkgs < <(printf '%s\n' "${packages[@]##*:}")
+
+DNF5_FORCE_INTERACTIVE=0 dnf5 install -yq --enable-repo="$repos_csv" "${pkgs[@]}"
 
 echo "::endgroup::"
